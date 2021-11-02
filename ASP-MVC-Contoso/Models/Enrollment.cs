3050b22b3832cc9bedfb0f8253d8ddfe8fcbd474
Tyronne282 <bradburntyronne@gmail.com>
2021-11-02 16:35:28 +0000

    Finished video tutorial

---
+++ ASP-MVC-Contoso/Models/Enrollment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_MVC_Contoso.Models
+{
+    public enum Grades
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grades? Grade { get; set; }
+
+        // Navigation properties
+        public virtual Course Course { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
